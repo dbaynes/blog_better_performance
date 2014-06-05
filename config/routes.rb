6c3073352e336f4cd546565d7fe7aa8f6aae1826
David Baynes <dlbaynes@gmail.com>
2014-06-04 21:51:10 -0700

    Done - attempts at better performance with eager loading and page caching

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+IvanTheTerriblesBlog::Application.routes.draw do
+  get 'posts/page/:page', to: 'posts#index'
+  resources :posts
+  resources :comments
+  resources :replies
+
+  root :to => 'posts#index'
+
+end
+
